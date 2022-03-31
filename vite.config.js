91489ee8ff53aa2bad8ec058fa8916c5fe772515
Daniel Orlov <43219752+opcei@users.noreply.github.com>
2022-04-01 02:05:33 +0300

    Update vite.config.js
    rm base: taskForm

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/taskForm/'
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
